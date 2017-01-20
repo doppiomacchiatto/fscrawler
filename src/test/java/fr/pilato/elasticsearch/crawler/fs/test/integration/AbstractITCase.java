5953f4eb741ca3b6e7d15c6c200f29850ceb222c
David Pilato <david@pilato.fr>
2017-01-20 18:59:39 +0100

    Add more traces

--- src/test/java/fr/pilato/elasticsearch/crawler/fs/test/integration/AbstractITCase.java
+++ src/test/java/fr/pilato/elasticsearch/crawler/fs/test/integration/AbstractITCase.java
@@ -127,6 +127,11 @@ public abstract class AbstractITCase extends AbstractFSCrawlerTestCase {
         elasticsearchClient.setElasticsearchBehavior();
 
         staticLogger.warn("started elasticsearch client: {}", elasticsearchClient);
+
+        Response response = elasticsearchClient.getClient().performRequest("GET", "/_nodes/stats/process?filter_path=**.max_file_descriptors");
+
+        String maxFileDescriptors = JsonUtil.asMap(response).toString();
+        staticLogger.warn("maxFileDescriptors={}", maxFileDescriptors);
     }
 
     private static boolean testClusterRunning(boolean withSecurity) throws IOException {
